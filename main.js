1e5cca6570aca892ab7174ba982d7db52d6bb703
Anna <honiara1951@gmail.com>
2019-02-19 22:31:27 +0200

    Рабочая версия для переноса на другой комп

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+const Filters = [
+  ['All', 15], ['Overdue', 0], ['Today', 0], ['Favorites', 7],
+  ['Repeating', 2], ['Tags', 6], ['Archive', 115]
+];
+
 const getFilterElement = (caption, amount=0, isChecked=false) => {
    return `
    <input
@@ -9,9 +14,32 @@ const getFilterElement = (caption, amount=0, isChecked=false) => {
    `;
 };
 
-const getTaskCard = (edit=true, color='black', repeat=false) => {
+/* не дописано
+Filters.forEach(([first, second]) => getFilterElement(first, second));
+*/
 
-}
+const getTaskCard = (color='black', repeat=false) => {
+  return `
+  <article
+  class="card card--${color}">
+  <form class="card__form" method="get">
+  <div class="card__inner">
+
+  <div class="card__color-bar">
+  <svg class="card__color-bar-wave" width="100%" height="10">
+  <use xlink:href="#wave"></use>
+  </svg>
+  </div>
+
+  <div class="card__textarea-wrap">
+  <label>
+  <textarea class="card__text"
+  placeholder="Start typing your text here..."
+  name="text">Here is a card with filled data</textarea>
+  </label>
+  </div>
+  `
+};
 
 /*<article class="card card--edit card--yellow card--repeat">
 <article class="card card--yellow card--deadline">
