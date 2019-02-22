2b2813846d35e2be63dbf58c443157d03539affd
Anna <honiara1951@gmail.com>
2019-02-22 20:27:23 +0200

    Исправила замечание наставника

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,15 @@
 const path = require(`path`);
-const dirname = path.resolve();
 
 module.exports = {
   mode: `development`,
   entry: `./src/main.js`,
   output: {
     filename: `bundle.js`,
-    path: path.join(dirname, `public`)
+    path: path.join(__dirname, `public`)
   },
   devtool: `source-map`,
   devServer: {
-    contentBase: path.join(dirname, `public`),
+    contentBase: path.join(__dirname, `public`),
     publicPath: `http://localhost:8080/`,
     hot: true,
     compress: true
